6eb4e7c9d29db0801a7aefa79af5ccfbe5ea9247
norco95 <norco95@gmail.com>
2017-11-15 09:48:09 +0200

    vehicleclass

--- CarAdvertiser/app/src/main/java/com/example/orbannorbert/caradvertiser/Vehicle.java
+++ CarAdvertiser/app/src/main/java/com/example/orbannorbert/caradvertiser/Vehicle.java
@@ -1,9 +1,21 @@
 package com.example.orbannorbert.caradvertiser;
 
+import java.util.ArrayList;
+
 /**
  * Created by OrbanNorbert on 2017. 11. 15..
  */
 
 public class Vehicle {
-    int a;
+    private String bodyType;
+    private String description;
+    private double engine;
+    private String fuelType;
+    private int horsePower;
+    private String id;
+    private ArrayList<String> images;
+    private int km;
+    private int numberOfDoors;
+    private double price;
+    
 }
